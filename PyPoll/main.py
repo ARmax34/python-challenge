7851176cb32d1a73d8f9b1341d36f8baa85ecb31
ARmax34 <a34rosenberg@gmail.com>
2022-12-28 01:13:45 -0500

    first part of the poll segment

---
+++ PyPoll/main.py
@@ -0,0 +1,10 @@
+# Import Dependencies
+import pandas as pd
+import numpy as np
+
+# Create reference to CSV file
+csv_budget = "Resources/election_data.csv"
+
+# Import the CSV into a pandas DataFrame
+election_df = pd.read_csv(csv_election)
+election_df
